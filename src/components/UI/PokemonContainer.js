1481eaf995fec24403140dbfa546108ca0dfd9f6
SusieHatter <susiivaldez@gmail.com>
2021-10-20 21:05:48 -0500

    Init commit

---
+++ src/components/UI/PokemonContainer.js
@@ -0,0 +1,11 @@
+import "./PokemonContainer.css";
+
+const Container = () => {
+  return (
+    <div className="container">
+      <img src="./images/pokedex.png" alt="pokedex" className="pokedex" />
+    </div>
+  );
+};
+
+export default Container;
