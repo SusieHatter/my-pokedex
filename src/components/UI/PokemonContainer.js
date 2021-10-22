b7f1a13ee29422a4509945b4e9681ef6261fe776
SusieHatter <susiivaldez@gmail.com>
2021-10-21 21:39:00 -0500

    -

--- src/components/UI/PokemonContainer.js
+++ src/components/UI/PokemonContainer.js
@@ -1,9 +1,11 @@
 import "./PokemonContainer.css";
 
+import "./PokemonCard";
+
 const Container = () => {
   return (
     <div className="container">
-      <img src="./images/pokedex.png" alt="pokedex" className="pokedex" />
+      <PokemonCard />
     </div>
   );
 };
---
+++ src/components/pokemon-list/PokemonCard.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+import "./PokemonCard.css";
+
+const Card = () => {
+  return <div></div>;
+};
+
+export default Card;
