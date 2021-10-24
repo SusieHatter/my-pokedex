e2fc7a17ac0a1441bc52917f13870ac16a545225
SusieHatter <susiivaldez@gmail.com>
2021-10-24 00:29:17 -0500

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import logo from "./images/logo.png";
 
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
   return (
     <div className="app">
+      <img className="logo" src={logo} alt="pokemon logo" />
       {Array.from({ length: 150 }).map((_, i) => {
         const id = i + 1;
         return <PokemonCard id={id} key={id} />;
