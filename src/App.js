86d8eae08d76a7fbf6db34ad0fe4084bdc0a4441
SusieHatter <susiivaldez@gmail.com>
2021-10-23 23:05:14 -0500

    Add logic to pokemon card to display different colors depending on each pokemon's type

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="app">
       {Array.from({ length: 150 }).map((_, i) => {
         const id = i + 1;
-        return <PokemonCard id={id} />;
+        return <PokemonCard id={id} key={id} />;
       })}
     </div>
   );
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,6 +1,22 @@
 import { usePokemon } from "../hooks/pokemon";
-
 import "./PokemonCard.css";
+
+const mapTypeToColor = {
+  fire: "#f57070d2",
+  grass: "#27a86ce3",
+  electric: "#fff45ecb",
+  water: "#6ebfe4e8",
+  ground: "#8b6b4ce0",
+  rock: "#929291d8",
+  fairy: "#ffa3e8d5",
+  poison: "#ad60bdd5",
+  bug: "#77d85fe5",
+  dragon: "#6f8bc0d2",
+  psychic: "#d6b16ce0",
+  flying: "#faa583c5",
+  fighting: "#f56685c9",
+  normal: "#a8a8a8dc",
+};
 
 const PokemonCard = ({ id }) => {
   const pokemon = usePokemon(id);
@@ -8,7 +24,10 @@ const PokemonCard = ({ id }) => {
     return <></>;
   }
   return (
-    <div className="pokemon-card">
+    <div
+      className="pokemon-card"
+      style={{ backgroundColor: mapTypeToColor[pokemon.types[0].type.name] }}
+    >
       <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       <p>{pokemon.name}</p>
     </div>
